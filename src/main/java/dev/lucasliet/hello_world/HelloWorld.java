7858efc14007572bdadb4e518e8cc9c24e4f1139
lucasliet <lucasouliveira@gmail.com>
2020-11-04 18:36:32 -0300

    change project name test

--- src/main/java/dev/lucasliet/hello_world/HelloWorld.java
+++ src/main/java/dev/lucasliet/hello_world/HelloWorld.java
@@ -1,7 +1,10 @@
 package dev.lucasliet.hello_world;
 
+import java.util.List;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		var words = List.of("Hello"," ","Dear"," ","World");
+		words.stream().map(word -> word.toUpperCase()).forEach(System.out::print);
 	}
 }
