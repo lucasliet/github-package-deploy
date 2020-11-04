f853d71fa5c09fb0d235acef85f3b17a50e2267a
lucasliet <lucasouliveira@gmail.com>
2020-11-04 18:09:47 -0300

    remove classpath test

--- src/main/java/dev/lucasliet/hello_world/HelloWorld.java
+++ src/main/java/dev/lucasliet/hello_world/HelloWorld.java
@@ -1,10 +1,7 @@
 package dev.lucasliet.hello_world;
 
-import java.util.List;
-
 public class HelloWorld {
 	public static void main(String[] args) {
-		List<String> words = List.of("Hello"," ", "dear", " ", "World");
-		words.stream().map(word -> word.toUpperCase()).forEach(System.out::print);;
+		System.out.println("Hello World");
 	}
 }
